202a225de218c5140f5198b8163a6f4f679a2a3d
Mingkai Ma <mamingkai824@gmail.com>
2018-04-02 16:10:34 +1000

    75_SortColors

---
+++ 75_SortColors.cc
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int hi = nums.size() - 1;
+        int mid = 0;
+        
+        while(mid <= hi)
+        {
+            switch(nums[mid])
+            {
+                case 0:
+                    swap(nums[low++], nums[mid++]);
+                    break;
+                
+                case 1:
+                    mid++;
+                    break;
+                
+                default:
+                    swap(nums[mid], nums[hi--]);
+                    break;
+            }
+        }
+    }
+};
