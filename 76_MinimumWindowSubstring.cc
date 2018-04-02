159526aedb89dea43210c65266ba23bb61ff6308
Mingkai Ma <mamingkai824@gmail.com>
2018-04-02 16:10:46 +1000

    76_MinimumWindowSubstring

---
+++ 76_MinimumWindowSubstring.cc
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+     
+        int lenS = s.size();
+        int lenT = t.size();
+        
+        if(lenS < lenT)
+            return "";
+        
+        int hash_t[256] = {0};
+        int hash_s[256] = {0};
+        
+        for(int i = 0; i < lenT; i++)
+            hash_t[t[i]]++;
+        
+        int start = 0;
+        int start_index = -1;
+        int min_len = INT_MAX;
+        
+        int count = 0;
+        for(int j = 0; j < lenS; j++)
+        {
+            hash_s[s[j]]++;
+            
+            if(hash_t[s[j]] != 0 && hash_s[s[j]] <= hash_t[s[j]])
+                count++;
+            
+            if(count == lenT)
+            {
+                while(hash_s[s[start]] > hash_t[s[start]] ||
+                     hash_t[s[start]] == 0)
+                {
+                    if(hash_s[s[start]] > hash_t[s[start]])
+                        hash_s[s[start]]--;
+                    start++;
+                }
+                
+                int lenWindow = j - start + 1;
+                if(min_len > lenWindow)
+                {
+                    min_len = lenWindow;
+                    start_index = start;
+                }
+            }
+        }
+        if(start_index == -1)
+            return "";
+        
+        return s.substr(start_index, min_len);
+    }
+};
