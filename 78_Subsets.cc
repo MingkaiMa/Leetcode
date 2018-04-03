93bc27e1c449f964d7e1c98af5c3374baa406cb3
Mingkai Ma <mamingkai824@gmail.com>
2018-04-03 15:13:37 +1000

    78_Subsets

---
+++ 78_Subsets.cc
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        
+        for(int i = 0; i < 1 << n; i++)
+        {
+            vector<int> t;
+            
+            for(int j = 0; j < n; j++)
+            {
+                if(i & 1 << j)
+                    t.push_back(nums[j]);
+            }
+            
+            res.push_back(t);
+        }
+        
+        return res;
+    }
+};
