611e7cae559c7c54a11a89449fe1c34eb50af344
Mingkai Ma <mamingkai824@gmail.com>
2018-08-01 13:52:51 +1000

    378 Binary Search

---
+++ 378_KthSmallestElementInASortedMatrixBinarySearch.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+
+        int n = matrix.size();
+        int left = matrix[0][0], right = matrix.back().back();
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            int cnt = 0;
+            for(int i = 0; i < n; i++){
+                cnt += (upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin());
+            }
+            if(cnt < k)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+};
