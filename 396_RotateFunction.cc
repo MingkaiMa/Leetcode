5d7a1bb663d0511902303464531644058ce13f4b
Mingkai Ma <mamingkai824@gmail.com>
2018-08-05 15:06:13 +1000

    3966

---
+++ 396_RotateFunction.cc
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void rotate(vector<int>& A){
+        int key = A[A.size() - 1];
+        for(int i = A.size() - 2; i >= 0; --i){
+            A[i + 1] = A[i];
+        }
+        A[0] = key;
+    }
+    int maxRotateFunction(vector<int>& A) {
+        
+        if(A.size() == 0)
+            return 0;
+        
+        if(A.size() == 1)
+            return 0;
+        
+        if(A.size() == 2){
+            int n1 = 0 * A[0] + 1 * A[1];
+            int n2 = 0 * A[1] + 1 * A[0];
+            if(n1 >= n2)
+                return n1;
+            return n2;
+        }
+        
+        
+        int res = INT_MIN;
+        
+        for(int i = 0; i < A.size(); i++){
+            // for(auto& a: A)
+            //     cout << a << ", ";
+            // cout << endl;
+            
+            int tmp = 0;
+            
+            for(int j = 0; j < A.size(); j++){
+                tmp = tmp + j * A[j];
+            }
+            
+            if(tmp > res)
+                res = tmp;
+            
+            rotate(A);
+        }
+        return res;
+    }
+};
