bc8b349404866f9988c478c7d8a09cf160d30ef3
Mingkai Ma <mamingkai824@gmail.com>
2018-08-09 11:32:29 +1000

    410_SplitArrayLargestSum.cc

---
+++ 410_SplitArrayLargestSum.cc
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int m) {
+        long long left = 0, right = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            left = max((int)left, nums[i]);
+            right += nums[i];
+        }
+        
+        while(left < right){
+            long long mid = left + (right - left) / 2;
+            if(canSplit(nums, m, mid))
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+    
+    bool canSplit(vector<int>& nums, int m, int sum){
+        int cnt = 1, curSum = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            curSum += nums[i];
+            if(curSum > sum){
+                curSum = nums[i];
+                ++cnt;
+                if(cnt > m)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
+
