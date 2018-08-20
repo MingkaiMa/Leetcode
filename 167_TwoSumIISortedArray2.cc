7382097d81f10638a5c25159b730026cee18f2de
Mingkai Ma <mamingkai824@gmail.com>
2018-08-20 22:33:24 +1000

    167_TwoSumIISortedArray2.cc

---
+++ 167_TwoSumIISortedArray2.cc
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res(2);
+        
+        int left = 0;
+        int right = numbers.size() - 1;
+        
+        while(left < right)
+        {
+            int sum = numbers[left] + numbers[right];
+            if(sum == target)
+            {
+                res[0] = left + 1;
+                res[1] = right + 1;
+                break;
+            }
+            
+            else if(sum > target)
+                right--;
+            else
+                left++;
+        }
+        
+
+        
+        return res;
+    }
+};
